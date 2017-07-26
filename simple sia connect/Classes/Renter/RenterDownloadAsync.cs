24e12a3649f882651cbe58c4b40455f3357b6967
Scott Guyton <kiljoy001@gmail.com>
2017-07-26 13:57:33 -0700

    Added RenterDownloadAsync

---
+++ simple sia connect/Classes/Renter/RenterDownloadAsync.cs
@@ -0,0 +1,29 @@
+﻿using simple_sia_connect.Classes.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace simple_sia_connect.Classes.Renter
+{
+    class RenterDownloadAsync :EndPointConnectAbstract
+    {
+        public RenterDownloadAsync(string siaAddress, string path)
+        {
+            Address = $"http://{siaAddress}/renter/downloadasync/{path}";
+        }
+
+        public RenterDownloadAsync(string path)
+        {
+            Address = $"http://localhost:9980/renter/downloadasync/{path}";
+        }
+
+        public override async Task<string> Connect(HttpClient client)
+        {
+            var returnValue = client.GetStringAsync(Address);
+            string msg = await returnValue;
+            return msg;
+        }
+    }
+}
